f944395f8b651f6a055f331266b6e951af7acc0b
Rafał Rzepecki <divided.mind@gmail.com>
2012-12-19 09:10:24 +0100

    Token verification.

--- lib/slosilo/key.rb
+++ lib/slosilo/key.rb
@@ -1,12 +1,13 @@
 require 'openssl'
 require 'json'
 require 'base64'
+require 'time'
 
 module Slosilo
   class Key
     def initialize raw_key = nil
       @key = if raw_key
-        OpenSSL::PKey.read raw_key
+        OpenSSL::PKey.read raw_key rescue raw_key
       else
         OpenSSL::PKey::RSA.new 2048
       end
@@ -39,14 +40,14 @@ module Slosilo
     end
     
     def sign value
-      case value
-      when Hash
-        sign value.to_a.sort
-      when String
-        sign_string value
-      else
-        sign value.to_json
-      end
+      sign_string(stringify value)
+    end
+    
+    SIGNATURE_LEN = 256
+    
+    def verify_signature data, signature
+      signature, salt = signature.unpack("A#{SIGNATURE_LEN}A*")
+      key.public_decrypt(signature) == hash_function.digest(salt + stringify(data))
     end
 
     # create a new timestamped and signed token carrying data
@@ -56,12 +57,29 @@ module Slosilo
       token
     end
     
+    def token_valid? token, expiry = 8 * 60
+      token = token.clone
+      signature = Base64::urlsafe_decode64(token.delete :signature)
+      (Time.parse(token[:timestamp]) + expiry > Time.now) && verify_signature(token, signature)
+    end
+    
     def sign_string value
       _salt = salt
       key.private_encrypt(hash_function.digest(_salt + value)) + _salt
     end
     
     private
+    def stringify value
+      case value
+      when Hash
+        value.to_a.sort.to_json
+      when String
+        value
+      else
+        value.to_json
+      end
+    end
+    
     def salt
       Slosilo::Random::salt
     end
