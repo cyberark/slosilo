348d8f1e3a0f59074a1e1d821c26f84edb5df099
Jon Mason <jonathan.j.mason@gmail.com>
2014-10-31 14:39:02 -0500

    add fingerprint to slosilo keystore if it doesn't exist

--- lib/slosilo/adapters/sequel_adapter/update_encryption.rb
+++ lib/slosilo/adapters/sequel_adapter/update_encryption.rb
@@ -16,6 +16,10 @@ module Slosilo
 
     def upgrade! db
 
+      unless fingerprint_in_db? db
+        add_fingerprint_to_db! db
+      end
+
       old_cipher = Slosilo::Migration::Symmetric.new
       new_cipher = Slosilo::Symmetric.new
       key = Slosilo::encryption_key || raise("Missing Slosilo::encryption_key!")
@@ -28,6 +32,29 @@ module Slosilo
         ctext = new_cipher.encrypt ptext, key: key, aad: row[:fingerprint]
         keystore.where(fingerprint: row[:fingerprint]).update(key: Sequel.blob(ctext))
         progress.increment
+      end
+    end
+
+    def fingerprint_in_db? db
+      db[keystore_table].columns.member? :fingerprint
+    end
+
+    def add_fingerprint_to_db db
+      db.transaction do
+        db.alter_table keystore_table do
+          add_column :fingerprint, String
+        end
+
+        db[keystore_table].each do |r|
+          pkey = OpenSSL::PKey.read r[:key]
+          fingerprint =  OpenSSL::Digest::MD5.hexdigest pkey.public_key.to_der
+          db[keystore_table].where(id: r[:id]).update(fingerprint: fingerprint)
+        end
+
+        db.alter_table keystore_table do
+          set_column_not_null :fingerprint
+          add_unique_constraint :fingerprint
+        end
       end
     end
 
