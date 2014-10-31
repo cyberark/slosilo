0d226745dc44aaefcd802bd3ae6054e7fc5743f9
Jon Mason <jonathan.j.mason@gmail.com>
2014-10-31 14:44:53 -0500

    actually, screw fingerprints

--- lib/slosilo/adapters/sequel_adapter/update_encryption.rb
+++ lib/slosilo/adapters/sequel_adapter/update_encryption.rb
@@ -1,7 +1,6 @@
 require 'sequel'
 require 'slosilo'
 require 'slosilo/migration'
-require 'slosilo/adapters/sequel_adapter/migration'
 
 module Slosilo
 
@@ -15,11 +14,6 @@ module Slosilo
     end
 
     def upgrade! db
-
-      unless fingerprint_in_db? db
-        add_fingerprint_to_db! db
-      end
-
       old_cipher = Slosilo::Migration::Symmetric.new
       new_cipher = Slosilo::Symmetric.new
       key = Slosilo::encryption_key || raise("Missing Slosilo::encryption_key!")
@@ -29,34 +23,12 @@ module Slosilo
 
       keystore.each  do |row|
         ptext = old_cipher.decrypt row[:key], key: key
-        ctext = new_cipher.encrypt ptext, key: key, aad: row[:fingerprint]
-        keystore.where(fingerprint: row[:fingerprint]).update(key: Sequel.blob(ctext))
+        ctext = new_cipher.encrypt ptext, key: key, aad: row[:id]
+        keystore.where(id: row[:id]).update(key: Sequel.blob(ctext))
         progress.increment
       end
     end
 
-    def fingerprint_in_db? db
-      db[keystore_table].columns.member? :fingerprint
-    end
-
-    def add_fingerprint_to_db! db
-      db.transaction do
-        db.alter_table keystore_table do
-          add_column :fingerprint, String
-        end
-
-        db[keystore_table].each do |r|
-          pkey = OpenSSL::PKey.read r[:key]
-          fingerprint =  OpenSSL::Digest::MD5.hexdigest pkey.public_key.to_der
-          db[keystore_table].where(id: r[:id]).update(fingerprint: fingerprint)
-        end
-
-        db.alter_table keystore_table do
-          set_column_not_null :fingerprint
-          add_unique_constraint :fingerprint
-        end
-      end
-    end
 
     def progress_bar count
       begin
