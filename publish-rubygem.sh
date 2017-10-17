9e8445b4b7371001c6f06b281c04571ba023ad4d
Dustin Collins <dustin.collins@cyberark.com>
2017-10-17 14:24:03 -0500

    gem name is slosilo

--- publish-rubygem.sh
+++ publish-rubygem.sh
@@ -6,6 +6,6 @@ docker run -i --rm -v $PWD:/src -w /src alpine/git clean -fxd
 
 summon --yaml "RUBYGEMS_API_KEY: !var rubygems/api-key" \
   docker run --rm --env-file @SUMMONENVFILE -v "$(pwd)":/opt/src \
-  registry.tld/conjurinc/publish-rubygem debify
+  registry.tld/conjurinc/publish-rubygem slosilo
 
 docker run -i --rm -v $PWD:/src -w /src alpine/git clean -fxd
