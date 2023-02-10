e4f3c65ce0d6af40c0ad4d5db661adeb272fbf9d
Micah Lee <micah.lee@cyberark.com>
2023-02-10 15:00:54 -0500

    Don't run git in a docker container
    Git is available on the Jenkins nodes, there's
    no need to run it in a container.

--- publish-rubygem.sh
+++ publish-rubygem.sh
@@ -2,10 +2,10 @@
 
 docker pull registry.tld/conjurinc/publish-rubygem
 
-docker run -i --rm -v $PWD:/src -w /src alpine/git clean -fxd
+git clean -fxd
 
 summon --yaml "RUBYGEMS_API_KEY: !var rubygems/api-key" \
   docker run --rm --env-file @SUMMONENVFILE -v "$(pwd)":/opt/src \
   registry.tld/conjurinc/publish-rubygem slosilo
 
-docker run -i --rm -v $PWD:/src -w /src alpine/git clean -fxd
+git clean -fxd
