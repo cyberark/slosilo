41af990e976fd02679f809fe044dc7bca78eb2e5
Bunny Remy <podmonkeys@gmail.com>
2023-10-24 14:28:47 -0400

    CONJSE-1802: Update publish-rubygem.sh

--- publish-rubygem.sh
+++ publish-rubygem.sh
@@ -1,13 +1,39 @@
+
+#!/usr/bin/env bash
+set -e
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <project>"
+  exit 1
+fi
+
+project="${1}"
+
+if [ ! -f "${project}.gemspec" ]; then
+  echo "Cannot find ${project}.gemspec"
+  echo "Usage: $0 <project>"
+  exit 1
+fi
+
+base="$(dirname "${0}")"
+
+docker run \
+  --rm \
+  --env RUBYGEMS_API_KEY \
+  --volume "$(pwd)":"$(pwd)" \
+  --workdir "$(pwd)" \
+  cyberark/ubuntu-ruby-builder:latest \
+  "${base}/publish-rubygem-container-entrpoint.sh" "${project}"
+
+
 #!/bin/bash -e
-
-echo "Bunny Test"
-
-docker pull registry.tld/conjurinc/publish-rubygem
-
-git clean -fxd
-
-summon --yaml "RUBYGEMS_API_KEY: !var rubygems/api-key" \
-  docker run --rm --env-file @SUMMONENVFILE -v "$(pwd)":/opt/src \
-  registry.tld/conjurinc/publish-rubygem slosilo
-
-git clean -fxd
+#
+#docker pull registry.tld/conjurinc/publish-rubygem
+#
+#git clean -fxd
+#
+#summon --yaml "RUBYGEMS_API_KEY: !var rubygems/api-key" \
+#  docker run --rm --env-file @SUMMONENVFILE -v "$(pwd)":/opt/src \
+#  registry.tld/conjurinc/publish-rubygem slosilo
+#
+#git clean -fxd
