03958979d76afdaaceaede3a6402d852eaee25e7
Bunny Remy <podmonkeys@gmail.com>
2023-10-26 15:57:25 -0400

    CONJSE-1802: Update publish-rubygem.sh

--- publish-rubygem.sh
+++ publish-rubygem.sh
@@ -8,10 +8,10 @@ git clone git@github.com:conjurinc/release-tools.git
 echo "==> Add $(pwd)/release-tools/bin to PATH"
 export PATH=$PATH:"$(pwd)/release-tools/bin"
 
-echo "==> check bin path"
-echo "${PATH}"
-ls -l "$(pwd)/release-tools/bin"
+#echo "==> check bin path"
+#echo "${PATH}"
+#ls -l "$(pwd)/release-tools/bin"
 
 #then call publish_rubygems script
 echo "==> Run Script: publish_rubygem "
-publish_rubygem slosilo
+publish-rubygem slosilo
