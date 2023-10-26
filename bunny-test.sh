bc532c76fd9a3830e205e4580421ee0d04a9a7b7
Bunny Remy <podmonkeys@gmail.com>
2023-10-26 15:52:19 -0400

    CONJSE-1802: Update publish-rubygem.sh

--- bunny-test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-git clone git@github.com:conjurinc/release-tools.git
-
-cat release-tools/bin/publish-rubygem
-
--- publish-rubygem.sh
+++ publish-rubygem.sh
@@ -1,52 +1,13 @@
-               
-#!/usr/bin/env bash
-set -ex
-
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <project>"
-  exit 1
-fi
-
-project="${1}"
-
-if [ ! -f "${project}.gemspec" ]; then
-  echo "Cannot find ${project}.gemspec"
-  echo "Usage: $0 <project>"
-  exit 1
-fi
-
-base="$(dirname "${0}")"
-echo "==> Base Dir = ${base}"
+#!/bin/bash -e
+set -e
 
 echo "==> Cloning Release-Tools GIT Repo"
 git clone git@github.com:conjurinc/release-tools.git
 
-echo "==> Doing the Docker Run"
-docker run \
-  --rm \
-  --env RUBYGEMS_API_KEY \
-  --volume "$(pwd)":"$(pwd)" \
-  --workdir "$(pwd)" \
-  cyberark/ubuntu-ruby-builder:latest \
-  sh -c "ls -la"
+#Add release-tools/bin to PATH
+echo "==> Add $(pwd)/release-tools/bin to PATH"
+export PATH=$PATH:"$(pwd)/release-tools/bin"
 
-#  sh -c "./release-tools/bin/build_and_run ${project}"
-  
-#  "./release-tools/bin/build_and_run" "${project}"
-echo "==> Done with Docker Run"
-
-#  "${base}/publish-rubygem-container-entrpoint.sh" "${project}"
-
-
-#OLD
-#!/bin/bash -e
-#
-#docker pull registry.tld/conjurinc/publish-rubygem
-#
-#git clean -fxd
-#
-#summon --yaml "RUBYGEMS_API_KEY: !var rubygems/api-key" \
-#  docker run --rm --env-file @SUMMONENVFILE -v "$(pwd)":/opt/src \
-#  registry.tld/conjurinc/publish-rubygem slosilo
-#
-#git clean -fxd
+#then call publish_rubygems script
+echo "==> Run Script: publish_rubygem "
+publish_rubygem slosilo
